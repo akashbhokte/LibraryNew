caf24241dccab18719dc2dcc3857d0de315d90fc
Akash Bhokte <akashbhokte4076@gmail.com>
2023-02-09 19:51:45 +0530

    commit test

--- App.js
+++ App.js
@@ -1,11 +1,6 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import Auth from './src/auth/Auth'
-import UserType from './src/screens/UserType'
-import BookList from './src/screens/BookList'
-import Transactions from './src/screens/Transactions'
-import SellerBookList from './src/screens/SellerBookList'
-import AddBook from './src/screens/AddBook'
 
 const App = () => {
   return (
